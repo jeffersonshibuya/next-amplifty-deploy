8bfed0ccb1f5741e6c21358c20b0f57ac57d7109
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2022-03-26 12:03:29 -0300

    Initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1>Production Site</h1>
+        <h1>Development Site</h1>
       </main>
 
       <footer className={styles.footer}>
